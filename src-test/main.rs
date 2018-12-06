2e7ee7092588ee663f91f2112cc870242d66cb7f
snsvrno <snsvrno@tuta.io>
2018-12-06 16:18:05 -0700

    feat: adds status indication
    after must struggling there is status indication.
    having issues with getting the total but it might
    be an issue with the site i was using for download
    testing.

--- src-test/main.rs
+++ src-test/main.rs
@@ -1,9 +1,17 @@
 extern crate download_lp as download;
 
+extern crate log;
+extern crate pretty_env_logger;
+
 static LINK : &'static str = "https://github.com/rust-lang/book/archive/one-book-to-rule-them-all.zip";
 
 fn main() {
+
+    // starts the loggers & sets the filter level for the logs
+    let mut builder = pretty_env_logger::formatted_builder();
+    builder
+        .filter(None,log::LevelFilter::Info )
+        .init();
+
     let result = download::download(LINK,".");
-    
-    println!("{:?}",result);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 extern crate reqwest;
-extern crate failure; use failure::Error;
+extern crate indicatif;
+#[macro_use] extern crate failure; use failure::Error;
 #[macro_use] extern crate log;
 
 use std::path::{ Path, PathBuf };
@@ -38,17 +39,49 @@ pub fn download<P:AsRef<Path>>(link : &str, path : P) -> Result<usize,Error>
         return Ok(0);
     }
 
-    // does the actual downloading.
-    let mut buffer : Vec<u8> = Vec::new();
+     let client = reqwest::Client::new();
+    let mut resp = client.get(link).send()?;
 
-    let mut response = reqwest::get(link)?;
-    response.copy_to(&mut buffer)?;
+    if resp.status().is_success() {
+        let len : u64 = resp.headers().get(reqwest::header::CONTENT_LENGTH)
+            .and_then(|l| l.to_str().ok())
+            .and_then(|l| l.parse().ok())
+            .unwrap_or(0);
+        
+        // let progress = indicatif::ProgressBar::new(len);
+        let progress = indicatif::ProgressBar::new_spinner();
+        // progress.println(format!("Downloading: {}",file));
+        progress.set_message(&format!("Downloading: {}",file));
 
-    // now writes it to a file
-    let mut disk_file = fs::File::create(&download_path)?;
-    let size = disk_file.write(&buffer)?;
+        let chunk_size = 1024usize;
 
-    info!("Download complete!");
-    
-    Ok(size)
+        let mut buffer : Vec<u8> = Vec::new();
+        let mut total : usize = 0;
+
+        loop {
+            let mut small_buffer = vec![0; chunk_size];
+            let small_buffer_read = resp.read(&mut small_buffer[..])?;
+            small_buffer.truncate(small_buffer_read);
+
+            match small_buffer.is_empty() {
+                true => break,
+                false => {
+                    buffer.extend(small_buffer);
+                    total += small_buffer_read;
+                    //progress.inc(small_buffer_read as u64);
+                    progress.set_message(&format!("{}.{} : {} / {}",file,ext,total,len));
+                },
+            }
+        }
+
+        let mut disk_file = fs::File::create(&download_path)?;
+        let size_disk = disk_file.write(&buffer)?;
+
+        progress.finish_with_message(&format!("{}.{} : Done",file,ext));
+
+        Ok(size_disk)
+
+    } else {
+        Err(format_err!("No response, are you connected to the internet?"))
+    }
 }
